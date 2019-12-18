a33894bdd5aa3a3c3156bd5be5d56ed466fc121e
Stéphane Le Hénanff <github@le-henanff.fr>
2019-12-18 09:37:00 +0100

    replace update_participants with set_participants_properties

---
+++ R/set_participant_properties.R
@@ -0,0 +1,20 @@
+#' set_participant_properties
+#'
+#' @param iSurveyID \dots
+#' @param tid \dots
+#' @param property \dots
+#' @param value \dots
+#'
+#' @export
+set_participant_properties <- function(survey_id, tid, property, value) {
+
+  limer::call_limer(
+    "set_participant_properties",
+    params = list(
+      "iSurveyID" = iSurveyID,
+      "aTokenQueryProperties" = tid,
+      "aTokenData" = as.list(stats::setNames(value, property))
+    )
+  )
+
+}
